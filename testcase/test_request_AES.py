02c77aedc5d9b60f0a9933117f2afe456266976c
yangdawei <davieyang@qq.com>
2019-11-01 15:45:10 +0800

    update new structure

--- testcase/test_request_AES.py
+++ testcase/test_request_AES.py
@@ -5,46 +5,91 @@ import unittest
 import json
 
 
-class AESGetEventListTest(unittest.TestCase):
+class AESTest(unittest.TestCase):
 
     def setUp(self):
-        # 字符串自动补全16倍数
         BS = 16
         self.pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 
         self.base_url = "http://127.0.0.1:8000/api/sec_get_guest_list/"
+        self.app_key = 'W7v4D60fds2Cmk2U'
 
     def encryptBase64(self,src):
-        """
-        生成 base64 字符串
-        """
         return base64.urlsafe_b64encode(src)
 
-    def encryptAES(self, src):
+    def encryptAES(self,src, key):
         """
         生成AES密文
         """
-        key = b'W7v4D60fds2Cmk2U'
         iv = b"1172311105789011"
         cryptor = AES.new(key, AES.MODE_CBC, iv)
-        src_str = self.pad(src)
-        src_byte = src_str.encode('utf-8')
-        ciphertext = cryptor.encrypt(src_byte)  # AES加密
-        aes_base64 = self.encryptBase64(ciphertext)  # base64 二次加密
-        return aes_base64
+        ciphertext = cryptor.encrypt(self.pad(src))
+        return self.encryptBase64(ciphertext)
 
     def test_aes_interface(self):
-        '''test AES interface'''
-        payload = {'eid':1, 'phone':''}
-        data = json.dumps(payload)
+        '''test aes interface'''
+        payload = {'eid': '1', 'phone': '13800138000'}
         # 加密
-        encoded = self.encryptAES(data).decode()
+        encoded = self.encryptAES(json.dumps(payload), self.app_key).decode()
 
         r = requests.post(self.base_url, data={"data": encoded})
         result = r.json()
-        print(result)
         self.assertEqual(result['status'], 200)
         self.assertEqual(result['message'], "success")
+
+    def test_get_guest_list_eid_null(self):
+        ''' eid 参数为空 '''
+        payload = {'eid': '','phone': ''}
+        encoded = self.encryptAES(json.dumps(payload), self.app_key).decode()
+
+        r = requests.post(self.base_url, data={"data": encoded})
+        result = r.json()
+        self.assertEqual(result['status'], 10021)
+        self.assertEqual(result['message'], 'eid cannot be empty')
+
+    def test_get_event_list_eid_error(self):
+        ''' 根据 eid 查询结果为空 '''
+        payload = {'eid': '901','phone': ''}
+        encoded = self.encryptAES(json.dumps(payload), self.app_key).decode()
+
+        r = requests.post(self.base_url, data={"data": encoded})
+        result = r.json()
+        self.assertEqual(result['status'], 10022)
+        self.assertEqual(result['message'], 'query result is empty')
+
+    def test_get_event_list_eid_success(self):
+        ''' 根据 eid 查询结果成功 '''
+        payload = {'eid': '1','phone': ''}
+        encoded = self.encryptAES(json.dumps(payload), self.app_key).decode()
+
+        r = requests.post(self.base_url, data={"data": encoded})
+        result = r.json()
+        self.assertEqual(result['status'], 200)
+        self.assertEqual(result['message'], 'success')
+        self.assertEqual(result['data'][0]['realname'],'张三')
+        self.assertEqual(result['data'][0]['phone'],'13800138000')
+
+    def test_get_event_list_eid_phone_null(self):
+        ''' 根据 eid 和phone 查询结果为空 '''
+        payload = {'eid':2,'phone':'10000000000'}
+        encoded = self.encryptAES(json.dumps(payload), self.app_key).decode()
+
+        r = requests.post(self.base_url, data={"data": encoded})
+        result = r.json()
+        self.assertEqual(result['status'], 10022)
+        self.assertEqual(result['message'], 'query result is empty')
+
+    def test_get_event_list_eid_phone_success(self):
+        ''' 根据 eid 和phone 查询结果成功 '''
+        payload = {'eid':1,'phone':'18633003301'}
+        encoded = self.encryptAES(json.dumps(payload), self.app_key).decode()
+
+        r = requests.post(self.base_url, data={"data": encoded})
+        result = r.json()
+        self.assertEqual(result['status'], 200)
+        self.assertEqual(result['message'], 'success')
+        self.assertEqual(result['data']['realname'],'alen')
+        self.assertEqual(result['data']['phone'],'18633003301')
 
 
 if __name__ == '__main__':
--- testcase/test_request_sign.py
+++ testcase/test_request_sign.py
@@ -22,40 +22,62 @@ class AddEventTest(unittest.TestCase):
         self.sign_md5 = md5.hexdigest()
 
     def test_add_event_sign_null(self):
-        ''' sign=null 签名参数为空 '''
-        now_time = str(int(self.client_time))
-        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':now_time,'sign':''}
+        ''' 签名参数为空 '''
+        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':'','sign':''}
+        r = requests.post(self.base_url, data=payload)
+        result = r.json()
+        self.assertEqual(result['status'], 10011)
+        self.assertEqual(result['message'], 'user sign null')
+
+    def test_add_event_time_out(self):
+        ''' 请求超时 '''
+        now_time = str(int(self.client_time) - 61)
+        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':now_time,'sign':'abc'}
         r = requests.post(self.base_url, data=payload)
         result = r.json()
         self.assertEqual(result['status'], 10012)
-        self.assertEqual(result['message'], 'user sign null')
+        self.assertEqual(result['message'], 'user sign timeout')
 
     def test_add_event_sign_error(self):
-        ''' sign='error' 签名参数错误 '''
-        now_time = str(int(self.client_time))
-        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':now_time,'sign':'error'}
-        r = requests.post(self.base_url, data=payload)
-        result = r.json()
-        self.assertEqual(result['status'], 10014)
-        self.assertEqual(result['message'], 'user sign error')
-
-    def test_add_event_sign_timeout(self):
-        ''' 签名参数超时 '''
-        now_time = str(int(self.client_time) - 61)
-        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':now_time,'sign':'error'}
+        ''' 签名错误 '''
+        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':self.client_time,'sign':'abc'}
         r = requests.post(self.base_url, data=payload)
         result = r.json()
         self.assertEqual(result['status'], 10013)
-        self.assertEqual(result['message'], 'user sign timeout')
+        self.assertEqual(result['message'], 'user sign error')
 
-    def test_add_event_sign_ok(self):
-        ''' 签名参数正确 '''
-        now_time = str(int(self.client_time))
-        payload = {'eid':1,'':'','limit':'','address':'','start_time':'','time':now_time,'sign':self.sign_md5}
+    def test_add_event_eid_exist(self):
+        ''' id已经存在 '''
+        payload = {'eid':1,'name':'一加4发布会','limit':2000,'address':"深圳宝体",'start_time':'2017','time':self.client_time,'sign':self.sign_md5}
         r = requests.post(self.base_url, data=payload)
         result = r.json()
-        self.assertEqual(result['status'], 10021)
-        self.assertEqual(result['message'], 'parameter error')
+        self.assertEqual(result['status'], 10022)
+        self.assertEqual(result['message'], 'event id already exists')
+
+    def test_add_event_name_exist(self):
+        ''' 名称已经存在 '''
+        payload = {'eid':11,'name':'一加3手机发布会','limit':2000,'address':"深圳宝体",'start_time':'2017','time':self.client_time,'sign':self.sign_md5}
+        r = requests.post(self.base_url,data=payload)
+        result = r.json()
+        self.assertEqual(result['status'], 10023)
+        self.assertEqual(result['message'], 'event name already exists')
+
+    def test_add_event_data_type_error(self):
+        ''' 日期格式错误 '''
+        payload = {'eid':11,'name':'一加5手机发布会','limit':2000,'address':"深圳宝体",'start_time':'2017','time':self.client_time,'sign':self.sign_md5}
+        r = requests.post(self.base_url,data=payload)
+        result = r.json()
+        self.assertEqual(result['status'], 10024)
+        self.assertIn('start_time format error.', result['message'])
+
+    def test_add_event_success(self):
+        ''' 添加成功 '''
+        payload = {'eid':11,'name':'一加4手机发布会','limit':2000,'address':"深圳宝体",'start_time':'2017-05-10 12:00:00','time':self.client_time,'sign':self.sign_md5}
+        r = requests.post(self.base_url,data=payload)
+        result = r.json()
+        self.assertEqual(result['status'], 200)
+        self.assertEqual(result['message'], 'add event success')
+
 
 if __name__ == '__main__':
     unittest.main()
